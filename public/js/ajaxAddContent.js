646c80b0d0e809a296625be98456817ba5ae10cf
大澤雄也 <yuyaosawa@raos-MacBook-Pro.local>
2023-08-15 18:13:02 +0900

    20230815 - フロント- 実装完了(仮)

--- public/js/ajaxAddContent.js
+++ public/js/ajaxAddContent.js
@@ -1,4 +1,3 @@
-
 // resources/js/infinite-scroll.js
 $(document).ready(function () {
     //let count = 0; // コンテンツの取得件数を初期化
@@ -27,35 +26,29 @@ function ajaxAddContent() {
 
     }).done(function (data) {
         $.each(data, function (key, val) {
-            //console.log(val.title);
-            //addContent += "<div>" + val.content + "</div>"; 書き方のイメージ
-            addContent += `<table class="p-5 mb-5" style="margin: 10px; background-color: #ffffff;">
+            if (val.del_flg != 1) {
+                //console.log(val.title);
+                //addContent += "<div>" + val.content + "</div>"; 書き方のイメージ
+                addContent += `<table class="p-5 mb-5" style="margin: 10px; background-color: #ffffff;">
             <tr>
 
                 <th scope='col' class="pr-3">
-                    @if($post->del_flg == 1)
-                    <h5 class="card-title">非表示済み</h5>
-                    <p class="card-text">この投稿は非表示にされています。</p>
-                    @else
+                   
                     <a href="{{ route('Post.show',${val.id}) }}">
-                        @csrf
-                        {{  ${val.title} }}
+                    ${val.title} 
                     </a>
-                    {{ ${val.date} }}
+                    ${val.date} 
                 </th>
             </tr>
 
             <tr>
                 <th scope='col' class="pl-3 pb-3">
-                    @if(empty(${val.image}))
                     画像の投稿はありません
-                    @elseif(!empty(${val.image}))
-                    <img src="{{ asset('storage/'.${val.image}) }}" style="width: 230px; height: 230px; object-fit: cover;">
-                    @endif
                 </th>
             </tr>
         </table>`;
-        })
+            }
+        });
 
         $("#append").append(addContent);
         count += data.length;
